bd1ae693cf270b44d6df9da36e8f3fc1aa579d5e
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-01 21:47:55 +0530

    hollow rectangle

--- switchCase.java
+++ switchCase.java
@@ -6,31 +6,17 @@ public class switchCase {
             System.out.print("Enter a day number (1-7): ");
             int day = sc.nextInt();
 
-            switch (day) {
-                case 1:
-                    System.out.println("Sunday");
-                    break;
-                case 2:
-                    System.out.println("Monday");
-                    break;
-                case 3:
-                    System.out.println("Tuesday");
-                    break;
-                case 4:
-                    System.out.println("Wednesday");
-                    break;
-                case 5:
-                    System.out.println("Thursday");
-                    break;
-                case 6:
-                    System.out.println("Friday");
-                    break;
-                case 7:
-                    System.out.println("Saturday");
-                    break;
-                default:
-                    System.out.println("Invalid day number.");
-            }
+            String dayName = switch (day) {
+                case 1 -> "Sunday";
+                case 2 -> "Monday";
+                case 3 -> "Tuesday";
+                case 4 -> "Wednesday";
+                case 5 -> "Thursday";
+                case 6 -> "Friday";
+                case 7 -> "Saturday";
+                default -> "Invalid day number.";
+            };
+            System.out.println(dayName);
         }
     }
 }
