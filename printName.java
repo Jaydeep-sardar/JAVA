f9b1a3f7ee65686daa11c51ef92083b10eaa23f6
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-01 21:51:38 +0530

    name printing

---
+++ printName.java
@@ -0,0 +1,9 @@
+public class printName {
+    public static void main(String[] args) {
+        printGivenName("Jaydeep"); // Change the name as needed
+    }
+
+    static void printGivenName(String name) {
+        System.out.println("Name: " + name);
+    }
+}
