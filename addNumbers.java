4a296eb202cc43324c75b171e2553321cb251a0f
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-01 21:52:26 +0530

    sum of two numbers

---
+++ addNumbers.java
@@ -0,0 +1,10 @@
+public class addNumbers {
+    public static void main(String[] args) {
+        int result = sum(5, 7); // Change numbers as needed
+        System.out.println("Sum: " + result);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+}
