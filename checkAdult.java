3781e77f97acf924b8e606dd978be1eebb3d9068
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-01 21:36:18 +0530

    condition statement

---
+++ checkAdult.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class checkAdult {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter age: ");
+            int age = sc.nextInt();
+            if (age >= 18) {
+                System.out.println("Person is an adult.");
+            } else {
+                System.out.println("Person is not an adult.");
+            }
+        }
