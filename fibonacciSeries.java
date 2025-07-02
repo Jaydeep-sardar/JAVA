59c9c744f58b9f4ea98fc43809f5ab074dbf6693
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-02 21:53:56 +0530

    java practice

---
+++ fibonacciSeries.java
@@ -0,0 +1,14 @@
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10; // Change this for more terms
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
