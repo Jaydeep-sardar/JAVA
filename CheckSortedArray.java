7c8fa68343e5718f0a541c173c6a5f4dd3920b41
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-07-09 23:55:11 +0530

    add CheckSortedArray implementation

---
+++ CheckSortedArray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CheckSortedArray {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number of elements: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter the elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            if (isSorted(arr)) {
+                System.out.println("The array is sorted in non-decreasing order.");
+            } else {
+                System.out.println("The array is not sorted.");
+            }
+        }
+    }
+
+    static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
